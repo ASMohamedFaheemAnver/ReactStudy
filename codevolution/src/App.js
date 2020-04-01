a2d35af776a2c7accb424eeeb143a5ad6df63cf3
rifacoder <jstrfaheem065@gmail.com>
2020-04-02 03:07:51 +0530

    setState,

--- codevolution/src/App.js
+++ codevolution/src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="RiFa">This is children props</Greet>
+      {/* <Greet name="RiFa">This is children props</Greet>
       <Greet name="FloveR">
         <button>Dynamic</button>
       </Greet>
       <Greet name="F"></Greet>
       <Welcome name="RiFa"></Welcome>
       <Welcome name="FloveR"></Welcome>
-      <Message></Message>
+      <Message></Message> */}
+      <Counter></Counter>
     </div>
   );
 }
