0d221371100e6ba6a876fca0cb48258587eba89c
rifacoder <jstrfaheem065@gmail.com>
2020-07-10 19:24:43 +0530

    Updating lifecycle,

--- codevolution/src/components/LifeCycleA.js
+++ codevolution/src/components/LifeCycleA.js
@@ -19,10 +19,29 @@ class LifeCycleA extends Component {
     console.log("LifecycleA componentDidMount!");
   }
 
+  shouldComponentUpdate() {
+    console.log("LifecycleA shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LifecycleA getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifecycleA componentDidUpdate");
+  }
+
+  changeState = () => {
+    this.setState({ name: "freedom" });
+  };
+
   render() {
     console.log("LifecycleA render!");
     return (
       <div>
+        <button onClick={this.changeState}>Change state</button>
         <LifeCycleB></LifeCycleB>
       </div>
     );
