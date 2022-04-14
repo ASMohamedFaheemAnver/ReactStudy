1bbea268ba61f94d1cabe1acb3ee5f49a77ddb8a
FreeDoM <jstrfaheem065@gmail.com>
2022-04-15 00:13:09 +0530

    Redir,

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
-import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
 
 function App() {
