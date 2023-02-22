acdaf6770727215da0a02e754958d1fec85f23dc
FreeDoM <jstrfaheem065@gmail.com>
2023-02-22 17:00:51 +0530

    Map heatmap and circles reference,

--- wftracker/src/App.js
+++ wftracker/src/App.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import Map from "./components/Map";
+import loadingIcon from "@iconify/icons-mdi/3d-rotation";
+import { Icon } from "@iconify/react";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getEvents = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events");
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(false);
+    } catch (e) {
+      console.log({ e });
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  console.log({ eventData, loading });
+
   return (
     <div>
-      <Map />
+      {!loading ? <Map eventData={eventData} /> : <Icon icon={loadingIcon} />}
     </div>
   );
 }
--- wftracker/src/components/Map.js
+++ wftracker/src/components/Map.js
@@ -1,20 +1,57 @@
 import GoogleMapReact from "google-map-react";
+import Marker from "./Marker";
 
-const Map = ({ center, zoom }) => {
+const Map = ({ center, zoom, eventData }) => {
+  const markers = eventData
+    .filter((event) => event?.categories[0]?.id === 8)
+    .map((event) => {
+      return {
+        id: event.id,
+        lat: event.geometries[0].coordinates[1],
+        lng: event.geometries[0].coordinates[0],
+      };
+    });
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+        bootstrapURLKeys={{
+          key: process.env.REACT_APP_GOOGLE_API_KEY,
+          libraries: ["visualization"],
+        }}
         defaultCenter={center}
         defaultZoom={zoom}
-      ></GoogleMapReact>
+        // heatmap={{
+        //   positions: markers,
+        //   options: {
+        //     radius: 20,
+        //     opacity: 1,
+        //   },
+        // }}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) =>
+          new maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.3,
+            map,
+            center: center,
+            radius: 800000,
+          })
+        }
+      >
+        {markers.map((marker) => {
+          return <Marker key={marker.id} lat={marker.lat} lng={marker.lng} />;
+        })}
+      </GoogleMapReact>
     </div>
   );
 };
 
 Map.defaultProps = {
   center: { lat: 0, lng: 0 },
-  zoom: 0,
+  zoom: 1,
 };
 
 export default Map;
