9e0caec4da825c9a359febad5f82c192c54d89ca
FreeDoM <jstrfaheem065@gmail.com>
2023-03-03 17:15:07 +0530

    Quill editor with custom file uploader,

--- richeditor/src/App.js
+++ richeditor/src/App.js
@@ -1,15 +1,47 @@
-import { useState } from "react";
-import ReactQuill from "react-quill";
+import { useMemo, useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
 import ReactMarkdown from "react-markdown";
 // import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 // import rehypeHighlight from "rehype-highlight";
 // import remarkBreaks from "remark-breaks";
 
+// #1 Import quill-image-uploader
+import ImageUploader from "quill-image-uploader";
+
+// #2 Register module
+Quill.register("modules/imageUploader", ImageUploader);
+
 const App = () => {
-  const [rText, setRText] = useState(
-    "# Hello\n\nThis is a **paragraph** with some `inline code`"
+  const [rText, setRText] = useState();
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote"],
+        [{ header: 1 }, { header: 2 }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ["link", "image"],
+      ],
+      imageUploader: {
+        upload: (file) => {
+          return new Promise((resolve, reject) => {
+            try {
+              // If we return right away it's giving error
+              setTimeout(() => {
+                resolve("hi");
+              }, 1000);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        },
+      },
+    }),
+    []
   );
+
   return (
     <div
       style={{
@@ -36,6 +68,7 @@ const App = () => {
       <ReactQuill
         value={rText}
         onChange={setRText}
+        modules={modules}
         style={{ height: 512, width: 512 }}
       />
     </div>
