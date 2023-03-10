cdacf2810196285800e086dc3b134e973b5c8c42
FreeDoM <jstrfaheem065@gmail.com>
2023-03-10 11:15:07 +0530

    Use effect unsubscribe working even in condition,

--- common/src/App.js
+++ common/src/App.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    if (true) {
+      console.log({ msg: "mounted" });
+      return () => {
+        console.log({ msg: "unmounted" });
+      };
+    }
+  }, []);
   return <></>;
 }
 
