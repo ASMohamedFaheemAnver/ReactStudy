11f0b0e363a10638afe5f8f138767ff3de82688a
FreeDoM <jstrfaheem065@gmail.com>
2023-04-21 10:33:36 +0530

    Half wheel added,

--- check/src/App.js
+++ check/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="body">
+      <div class="container">
+        <div class="one">1</div>
+        <div class="two">2</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
