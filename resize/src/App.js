1a9eb5840dc6cf1f7539ba1c3a9fbf74163bf27f
FreeDoM <jstrfaheem065@gmail.com>
2023-05-19 10:20:45 +0530

    Resizable,

---
+++ resize/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="resizable">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+      </div>
+      <textarea></textarea>
+    </div>
+  );
+}
+
+export default App;
