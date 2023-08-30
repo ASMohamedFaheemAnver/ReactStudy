819040156fa4b4013874375b6f2ef5c3ddc8797a
FreeDoM <jstrfaheem065@gmail.com>
2023-08-30 14:22:21 +0530

    Download files,

--- download/src/App.js
+++ download/src/App.js
@@ -1,11 +1,12 @@
 import FileSaver from "file-saver";
 import { imageUri } from "./config";
+import axios from "axios";
 
 function App() {
   const onDownload = async () => {
-    const response = await fetch(imageUri);
-    const blob = await response.blob();
-    FileSaver.saveAs(blob);
+    // const response = await fetch(imageUri);
+    const response = await axios.get(imageUri, { responseType: "blob" });
+    FileSaver.saveAs(response.data);
   };
   return <button onClick={onDownload}>Download</button>;
 }
