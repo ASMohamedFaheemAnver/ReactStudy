9f149751cff98021fdd88577ed4bdfd7c3bbc389
FreeDoM <jstrfaheem065@gmail.com>
2023-09-12 15:41:07 +0530

    No need to register since library requires it,

--- rpush/public/firebase-messaging-sw.js
+++ rpush/public/firebase-messaging-sw.js
@@ -2,12 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register(`firebase-messaging-sw.js`)
-    .then(function (registration) {})
-    .catch(function (err) {});
-}
+
+// firebase-cloud-messaging-push-scope requiring this automatically so we don't need to register this file at all, we just need to initialize firebase.
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register(`firebase-messaging-sw.js`)
+//     .then(function (registration) {})
+//     .catch(function (err) {});
+// }
 firebase.initializeApp({
   apiKey: "AIzaSyCidBU0K5ujoAeJqVFYem3r4SdDp5AfXQc",
   authDomain: "codersauthoritystudy.firebaseapp.com",
