b9380da6fa798d05f9525d43ce028955ceaa6a4c
FreeDoM <jstrfaheem065@gmail.com>
2023-09-12 15:50:06 +0530

    Hide config anyway,

--- rpush/public/firebase-messaging-sw.js
+++ rpush/public/firebase-messaging-sw.js
@@ -2,6 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
+importScripts(`${location.origin}/config.js`);
 
 // firebase-cloud-messaging-push-scope requiring this automatically so we don't need to register this file at all, we just need to initialize firebase.
 // if ("serviceWorker" in navigator) {
@@ -10,13 +11,5 @@ importScripts(
 //     .then(function (registration) {})
 //     .catch(function (err) {});
 // }
-firebase.initializeApp({
-  apiKey: "AIzaSyCidBU0K5ujoAeJqVFYem3r4SdDp5AfXQc",
-  authDomain: "codersauthoritystudy.firebaseapp.com",
-  projectId: "codersauthoritystudy",
-  storageBucket: "codersauthoritystudy.appspot.com",
-  messagingSenderId: "407357514835",
-  appId: "1:407357514835:web:cf99717c5cef6319b612a6",
-  measurementId: "G-RERN94TGD1",
-});
+firebase.initializeApp(config);
 const initMessaging = firebase.messaging();
