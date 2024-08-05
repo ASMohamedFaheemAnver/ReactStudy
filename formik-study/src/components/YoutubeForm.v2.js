a6931f4fa306c232f16dcab4245bc7173afbe682
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-08-05 14:37:56 +0530

    Customize error message,

--- formik-study/src/components/YoutubeForm.v2.js
+++ formik-study/src/components/YoutubeForm.v2.js
@@ -1,6 +1,7 @@
 import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
 import React from "react";
 import * as Yup from "yup";
+import TextError from "./TextError";
 
 const YoutubeForm = () => {
   const validationSchema = Yup.object({
@@ -33,13 +34,17 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="text" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {(errorMessage) => {
+              return <TextError>{errorMessage}</TextError>;
+            }}
+          </ErrorMessage>
         </div>
 
         <div className="form-control">
