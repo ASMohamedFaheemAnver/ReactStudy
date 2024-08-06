2c0b45cb4cfd0c16f8622f8b2db0e961ba1e7d99
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-08-06 21:06:00 +0530

    Formik custom controls and validation,

--- formik-study/src/App.js
+++ formik-study/src/App.js
@@ -1,26 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import YoutubeForm from "./components/YoutubeForm.v2";
-import Parent from "./components/Parent";
-import { memo } from "react";
+import FormikContainer from "./reuable-components/FormikContainer";
 
 function App() {
   return (
     <div className="App">
-      <Parent children={Child}>
-        {/* {memo((props) => {
-          console.log({ props });
-          return <p>Yo</p>;
-        })} */}
-      </Parent>
-      <YoutubeForm />
+      <FormikContainer />
     </div>
   );
 }
-
-const Child = memo((props) => {
-  console.log({ props });
-  return <p>I am just a child :(</p>;
-});
 
 export default App;
